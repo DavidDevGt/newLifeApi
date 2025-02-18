24d31aaea521349e9ae5ddd137016c26836c414e
DavidDevGt <josuedavidvl18@gmail.com>
2025-02-18 01:23:46 -0600

    update pipeline

--- public/index.php
+++ public/index.php
@@ -9,7 +9,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->safeLoad();
 
-// CORS | JSON Format Headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=UTF-8');
 
