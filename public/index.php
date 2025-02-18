975f77f2d750ba750e316a2c50d602d00d1f1802
DavidDevGt <josuedavidvl18@gmail.com>
2025-02-18 01:08:39 -0600

    testing deploy

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->safeLoad();
 
-// CORS | JSON Format Headers
+// CORS | JSON Format
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=UTF-8');
 
