9258da8751aa0aa7e22deb449c340dfff6790513
DavidDevGt <josuedavidvl18@gmail.com>
2025-02-18 03:11:55 -0600

    cors updated config

--- public/index.php
+++ public/index.php
@@ -9,7 +9,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->safeLoad();
 
-header('Access-Control-Allow-Origin: *');
+// CORS
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, X-Api-Key");
+header("Access-Control-Allow-Credentials: true");
+
+
 header('Content-Type: application/json; charset=UTF-8');
 
 require __DIR__ . '/../app/Routes/web.php';
